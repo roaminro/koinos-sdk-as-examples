03afd9fe0c988364b390dcc21e5dbaaf2c2aca15
roaminroe <roamin@tuta.io>
2022-01-31 17:39:58 -0500

    update koinos-as-gen and Calculator contract

--- calculator/assembly/Calculator.ts
+++ calculator/assembly/Calculator.ts
@@ -1,20 +1,43 @@
 import { calculator } from "./proto/calculator";
 
 export class Calculator {
-
   add(args: calculator.add_arguments): calculator.add_result {
-    return new calculator.add_result(args.x + args.y);
+    const x = args.x;
+    const y = args.y;
+
+    const res = new calculator.add_result();
+    res.value = x + y;
+
+    return res;
   }
 
   sub(args: calculator.sub_arguments): calculator.sub_result {
-    return new calculator.sub_result(args.x - args.y);
+    const x = args.x;
+    const y = args.y;
+
+    const res = new calculator.sub_result();
+    res.value = x - y;
+
+    return res;
   }
 
   mul(args: calculator.mul_arguments): calculator.mul_result {
-    return new calculator.mul_result(args.x * args.y);
+    const x = args.x;
+    const y = args.y;
+
+    const res = new calculator.mul_result();
+    res.value = x * y;
+
+    return res;
   }
 
   div(args: calculator.div_arguments): calculator.div_result {
-    return new calculator.div_result(args.x / args.y);
+    const x = args.x;
+    const y = args.y;
+
+    const res = new calculator.div_result();
+    res.value = x / y;
+
+    return res;
   }
 }
