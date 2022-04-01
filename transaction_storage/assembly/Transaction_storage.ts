30ef7a80d33ec3440f3900e35aa48f7a465751f2
roaminroe <roamin@tuta.io>
2022-04-01 00:22:58 -0400

    add basic unit tests to transaction_storage contract

--- transaction_storage/assembly/Transaction_storage.ts
+++ transaction_storage/assembly/Transaction_storage.ts
@@ -2,15 +2,36 @@ import { chain, protocol, System } from "koinos-as-sdk";
 import { transaction_storage } from "./proto/transaction_storage";
 
 export class Transaction_storage {
+  _space: chain.object_space;
+
+  constructor() {
+    this._space = new chain.object_space(false, System.getContractId(), 0);
+
+  }
+
   store_transaction(
     args: transaction_storage.store_transaction_arguments
   ): transaction_storage.store_transaction_result {
     const transaction = args.transaction!;
 
-    const space = new chain.object_space(false, System.getContractId(), 0);
-    System.putObject(space, transaction.id!, transaction, protocol.transaction.encode);
-
+    System.putObject(this._space, transaction.id!, transaction, protocol.transaction.encode);
 
     return new transaction_storage.store_transaction_result();
+  }
+
+  get_transaction(
+    args: transaction_storage.get_transaction_arguments
+  ): transaction_storage.get_transaction_result {
+    const id = args.id!;
+
+    const res = new transaction_storage.get_transaction_result();
+
+    const tx = System.getObject<Uint8Array, protocol.transaction>(this._space, id, protocol.transaction.decode);
+
+    if (tx) {
+      res.value = tx;
+    }
+
+    return res;
   }
 }
---
+++ transaction_storage/assembly/__tests__/transaction_storage.spec.ts
@@ -0,0 +1,44 @@
+import { Base58, chain, protocol, System, Arrays, MockVM } from "koinos-as-sdk";
+import { Transaction_storage } from "../Transaction_storage";
+import { transaction_storage } from "../proto/transaction_storage";
+
+const CONTRACT_ID = Base58.decode("1DQzuCcTKacbs9GGScRTU1Hc8BsyARTPqe");
+const SPACE = new chain.object_space(false, CONTRACT_ID, 0);
+
+describe("transaction_storage", () => {
+  beforeEach(() => {
+    MockVM.reset();
+    MockVM.setContractId(CONTRACT_ID);
+  });
+
+  it("should store a transaction", () => {
+    const contract = new Transaction_storage();
+    const txId = Arrays.fromHexString('0x123456');
+    const tx = new protocol.transaction(txId);
+
+    const args = new transaction_storage.store_transaction_arguments(tx);
+    contract.store_transaction(args);
+
+    const storedTx = System.getObject<Uint8Array, protocol.transaction>(SPACE, txId, protocol.transaction.decode);
+
+    expect(storedTx).not.toBeNull();
+    expect(Arrays.equal(storedTx!.id, txId)).toBe(true);
+  });
+
+  it("should get a transaction", () => {
+    const contract = new Transaction_storage();
+    const txId = Arrays.fromHexString('0x123456');
+    const tx = new protocol.transaction(txId);
+
+    const storeArgs = new transaction_storage.store_transaction_arguments(tx);
+    contract.store_transaction(storeArgs);
+
+    const getArgs = new transaction_storage.get_transaction_arguments(txId);
+
+    const storedTx = contract.get_transaction(getArgs);
+
+    expect(storedTx.value).not.toBeNull();
+    expect(Arrays.equal(storedTx.value!.id, txId)).toBe(true);
+  });
+
+});
