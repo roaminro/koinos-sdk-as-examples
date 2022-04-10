efa4492267a706c46c4b551c8af4134e21d5d98f
Julian Gonzalez <joticajulian@gmail.com>
2022-04-11 00:20:43 +0200

    draft verify and add authority

--- wallet/assembly/Wallet.ts
+++ wallet/assembly/Wallet.ts
@@ -1,4 +1,4 @@
-import { Arrays, authority, Protobuf, System, SafeMath } from "koinos-as-sdk";
+import { Arrays, authority, Protobuf, System, Crypto, SafeMath } from "koinos-as-sdk";
 import { wallet } from "./proto/wallet";
 import { State } from "./State";
 
@@ -12,14 +12,47 @@ export class Wallet {
     this._state = new State(this._contractId);
   }
 
+  _equalBytes(a: Uint8Array, b: Uint8Array): boolean {
+    if (a.length != b.length) return false;
+    for (let i = 0; i < a.length; i++ ) {
+      if (a[i] != b[i]) return false;
+    }
+    return true;
+  }
+
   _verify(name: string): boolean {
+    const auth = this._state.getAuthority(name);
+    // if (!auth) exit
+    
     const signatures = System.getTransactionField("signatures");
-    const txId = System.getTransactionField("id");
-    const addresses: Uint8Array[] = [];
+    const txId = System.getTransactionField("id");    
+    
+    const totalWeight: u32 = 0;
     for(let i = 0; i < signatures.length; i++) {
       const publicKey = System.recoverPublicKey(0, signatures[i], txId);
-      const address = new Uint8Array(0); // todo: get address from public key
-      addresses.push(address);
+      const address = Crypto.addressFromPublicKey(publicKey);
+
+      for(let j = 0; j < auth.key_auths.length; j++) {
+        const keyAddress = auth.key_auths[j].address;
+        if(this._equalBytes(address, keyAddress)) {
+          totalWeight += auth.key_auths[j].weight;
+        }  
+      }
     }
+    return totalWeight >= auth.weight_threshold;
+  }
+
+  addAuthority(args: wallet.add_authority_arguments): wallet.add_authority_result {
+    const name = args.name!;
+    const auth = args.authority;
+
+    const authNames = this._state.getAuthorityNames();
+    if (authNames.length == 0) {
+      // todo: insert owner
+    }
+    if (!this._verify("owner")) {
+      // todo: throw error
+    }
+    // todo: add authority
   }
 }
