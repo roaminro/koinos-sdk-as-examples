3af67fcc3d70f534ceb86b91ee91857a09b97cc3
Julian Gonzalez <joticajulian@gmail.com>
2022-04-11 23:03:38 +0000

    authorize function, fix compilation

--- wallet/assembly/State.ts
+++ wallet/assembly/State.ts
@@ -1,37 +1,47 @@
-import { chain, System } from "koinos-as-sdk";
+import { authority, chain, System, Protobuf } from "koinos-as-sdk";
 import { wallet } from "./proto/wallet";
 
 const AUTHORITIES_SPACE_ID = 0;
 const AUTHORITY_NAMES_SPACE_ID = 1;
+const PROTECTED_CONTRACTS_SPACE_ID = 2;
 const AUTHORITY_NAMES_KEY = new Uint8Array(0);
 
 export class State {
   contractId: Uint8Array;
   authoritiesSpace: chain.object_space;
   authorityNamesSpace: chain.object_space;
+  protectedContractsSpace: chain.object_space;
 
   constructor(contractId: Uint8Array) {
     this.contractId = contractId;
 
     this.authoritiesSpace = new chain.object_space(false, contractId, AUTHORITIES_SPACE_ID);
     this.authorityNamesSpace = new chain.object_space(false, contractId, AUTHORITY_NAMES_SPACE_ID);
+    this.protectedContractsSpace = new chain.object_space(false, contractId, PROTECTED_CONTRACTS_SPACE_ID);
   }
 
   setAuthority(name: string, authority: wallet.authority): void {
     System.putObject(this.authoritiesSpace, name, authority, wallet.authority.encode);
   }
 
-  getAuthority(name: string): wallet.authority {
+  getAuthority(name: string): wallet.authority | null {
     const authority = System.getObject<String, wallet.authority>(this.authoritiesSpace, name, wallet.authority.decode);
-    return authority ? authority : new wallet.authority();
+    return authority;
   }
 
   setAuthorityNames(authNames: wallet.authority_names): void {
-    System.putObject(this.authorityNamesSpace, AUTHORITY_NAMES_KEY, authNames, wallet.authority_names);
+    System.putObject(this.authorityNamesSpace, AUTHORITY_NAMES_KEY, authNames, wallet.authority_names.encode);
   }
 
   getAuthorityNames(): wallet.authority_names {
-    const authNames = System.getObject<Uint8Array, wallet.authority_names>(this.authorityNamesSpace, AUTHORITY_NAMES_KEY, wallet.authority_names);
+    const authNames = System.getObject<Uint8Array, wallet.authority_names>(this.authorityNamesSpace, AUTHORITY_NAMES_KEY, wallet.authority_names.decode);
     return authNames ? authNames : new wallet.authority_names();
+  }
+
+  getAuthorityProtectedContract(call: authority.call_target, entireContract: boolean): wallet.authority_contract | null {
+    const protectedContract = new wallet.protected_contract(call.contract_id, call.entry_point, entireContract);
+    const key = Protobuf.encode(protectedContract, wallet.protected_contract.encode);
+    const authority = System.getObject<Uint8Array, wallet.authority_contract>(this.protectedContractsSpace, key, wallet.authority_contract.decode);
+    return authority;
   }
 }
--- wallet/assembly/Wallet.ts
+++ wallet/assembly/Wallet.ts
@@ -1,4 +1,4 @@
-import { Arrays, authority, Protobuf, System, Crypto, SafeMath } from "koinos-as-sdk";
+import { Arrays, authority, value, Protobuf, System, Crypto, SafeMath } from "koinos-as-sdk";
 import { wallet } from "./proto/wallet";
 import { State } from "./State";
 
@@ -20,39 +20,95 @@ export class Wallet {
     return true;
   }
 
-  _verify(name: string): boolean {
+  _requireAuthority(name: string): void {
     const auth = this._state.getAuthority(name);
-    // if (!auth) exit
+    if (auth == null) {
+      System.log(`invalid authority '${name}'`);
+      System.exitContract(1);
+      return;
+    }
     
-    const signatures = System.getTransactionField("signatures");
-    const txId = System.getTransactionField("id");    
+    const sigBytes = System.getTransactionField("signatures")!.message_value!.value!;
+    const signatures = Protobuf.decode<value.list_type>(sigBytes, value.list_type.decode);    
+    const txId = System.getTransactionField("id")!.bytes_value!;    
     
-    const totalWeight: u32 = 0;
-    for(let i = 0; i < signatures.length; i++) {
-      const publicKey = System.recoverPublicKey(0, signatures[i], txId);
-      const address = Crypto.addressFromPublicKey(publicKey);
+    let totalWeight: u32 = 0;
+    for(let i = 0; i < signatures.values.length; i++) {
+      const publicKey = System.recoverPublicKey(signatures.values[i].bytes_value!, txId);
+      const address = Crypto.addressFromPublicKey(publicKey!);
 
       for(let j = 0; j < auth.key_auths.length; j++) {
-        const keyAddress = auth.key_auths[j].address;
+        const keyAddress = auth.key_auths[j].address!;
         if(this._equalBytes(address, keyAddress)) {
           totalWeight += auth.key_auths[j].weight;
         }  
       }
     }
-    return totalWeight >= auth.weight_threshold;
+    
+    if (totalWeight < auth.weight_threshold) {
+      System.log(`authority ${name} failed`);
+      System.exitContract(1);
+    }
   }
 
-  addAuthority(args: wallet.add_authority_arguments): wallet.add_authority_result {
-    const name = args.name!;
-    const auth = args.authority;
-
+  add_authority(args: wallet.add_authority_arguments): wallet.add_authority_result {
     const authNames = this._state.getAuthorityNames();
-    if (authNames.length == 0) {
-      // todo: insert owner
+    if (authNames.names.length == 0 && args.name != "owner") {
+      System.log("The first authority must be 'owner'");
+      System.exitContract(1);
     }
-    if (!this._verify("owner")) {
-      // todo: throw error
+
+    if (authNames.names.includes(args.name!)) {
+      System.log(`Authority ${args.name!} already exists`);
+      System.exitContract(1);
     }
-    // todo: add authority
+    
+    this._requireAuthority("owner");
+    this._state.setAuthority(args.name!, args.authority!);
+    authNames.names.push(args.name!);
+    this._state.setAuthorityNames(authNames);
+    return new wallet.add_authority_result(true);
   }
+
+  // todo: update authority function
+  // todo: delete authority function
+
+  authorize(args: authority.authorize_arguments): authority.authorize_result {
+    if (args.type == authority.authorization_type.contract_call) {
+      // check if there is an authority for the specific endpoint
+      let authContract = this._state.getAuthorityProtectedContract(args.call!, false);
+      if (!authContract) {
+        // check if there is an authority for the entire contract
+        authContract = this._state.getAuthorityProtectedContract(args.call!, true);
+        if (!authContract) {
+          this._requireAuthority("owner"); // todo: change to "active"
+          return new authority.authorize_result(true);
+        }
+      }
+
+      if (authContract.native != null) {
+        this._requireAuthority(authContract.native);
+        return new authority.authorize_result(true);
+      }
+
+      if (authContract.external != null) {
+        const contractId = authContract.external.contract_id!;
+        const entryPoint = authContract.external.entry_point;
+        const resBuf = System.callContract(contractId, entryPoint, Protobuf.encode(args, authority.authorize_arguments.encode))!;
+        const authorized = Protobuf.decode<authority.authorize_result>(resBuf, authority.authorize_result.decode);
+        return authorized;
+      }
+
+      System.log("Authority is not defined correctly");
+      return new authority.authorize_result(false);
+    }
+    // todo: type upload_contract
+    // todo: type transaction_application
+    return new authority.authorize_result(false);
+  }
+
+  // todo: authorize update authority protected contract
+  // todo: request update (clock starts)
+  // todo: add protected contract
+  // todo: update protected contract
 }
